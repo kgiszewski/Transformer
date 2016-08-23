0ac218e7533d3a0b33827fe129cd87d1a4bbf6bb
kgiszewski <kevin@giszewski.com>
2016-08-23 08:05:29 -0400

    Init

---
+++ src/Transformer/ConsoleBuildEngine.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using Microsoft.Build.Framework;
+
+namespace Transformer
+{
+    public class ConsoleBuildEngine : IBuildEngine
+    {
+        public int ColumnNumberOfTaskNode
+        {
+            get { return 0; }
+        }
+
+        public bool ContinueOnError
+        {
+            get { return true; }
+        }
+
+        public int LineNumberOfTaskNode
+        {
+            get { return 0; }
+        }
+
+        public string ProjectFileOfTaskNode
+        {
+            get { return ""; }
+        }
+
+        public bool BuildProjectFile(string projectFileName, string[] targetNames, IDictionary globalProperties,
+            IDictionary targetOutputs)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogCustomEvent(CustomBuildEventArgs e)
+        {
+            Console.WriteLine("[Custom] " + e.Message);
+        }
+
+        public void LogErrorEvent(BuildErrorEventArgs e)
+        {
+            Console.WriteLine("[Error] " + e.Message);
+        }
+
+        public void LogMessageEvent(BuildMessageEventArgs e)
+        {
+            Console.WriteLine("[Message] " + e.Message);
+        }
+
+        public void LogWarningEvent(BuildWarningEventArgs e)
+        {
+            Console.WriteLine("[Warn] " + e.Message);
+        }
+    }
+}
---
+++ src/Transformer/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Transformer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xmlTransformer = new Microsoft.Web.Publishing.Tasks.TransformXml();
+            
+            xmlTransformer.Source = args[0];
+            xmlTransformer.Destination = args[0];
+            xmlTransformer.Transform = args[1];
+            xmlTransformer.BuildEngine = new ConsoleBuildEngine();
+
+            xmlTransformer.Execute();
+        }
+    }
+}
